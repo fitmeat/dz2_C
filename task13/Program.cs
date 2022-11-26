2fb01a7f407fe7b81c6065f4233f7ea67b556474
fitmeat <eenot825@gmail.com>
2022-11-26 16:12:08 +0300

    Выполнил задание 13

---
+++ task13/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число");//645
+int n = int.Parse(Console.ReadLine());
+if(n<100 && n>-100){
+    Console.WriteLine("Третьей цифры нет");
+} 
+else{
+    int third_n = n%10;
+    Console.WriteLine($"Третья цифра числа - {third_n}");
+}
