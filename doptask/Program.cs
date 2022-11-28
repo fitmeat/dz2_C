8415d8fc1917d359412922c90b2c90857cdba371
fitmeat <eenot825@gmail.com>
2022-11-28 21:04:41 +0300

    Выполнил дополнительное задание

---
+++ doptask/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число");
+int n = int.Parse(Console.ReadLine());
+int max1 = n, max2 = -1;
+while(n!=0){
+    n = int.Parse(Console.ReadLine());
+    if(n>max1){
+        max2 = max1;
+        n = max1;
+    } else if(n>max2){
+        max2 = n;
+    }
+}
+Console.WriteLine($"max2 = {max2}");
